4c1837477839cae9e48c3f1cb8c955d323901838
kanegongoon <101990076+kanegongoon@users.noreply.github.com>
2022-04-25 22:03:00 +0900

    修正点の編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: :edit
 
 
@@ -36,9 +36,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if current_user.id == item.user_id
-      redirect_to root_path item.destroy
+    if current_user.id == @item.user_id
+      @item.destroy
+      redirect_to root_path
     else
       render :show
     end
